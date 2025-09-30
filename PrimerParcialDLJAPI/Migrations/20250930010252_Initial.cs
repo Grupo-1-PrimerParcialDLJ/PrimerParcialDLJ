ff5d26fd1d6a57676f7bfd7fd616ad135b554e33
Laura Ortiz <a2024111203@estudiantes.upsa.edu.bo>
2025-09-29 21:46:05 -0400

    Arreglar Migrations (nombres)

--- PrimerParcialDLJAPI/Migrations/20250930010252_Initial.cs
+++ PrimerParcialDLJAPI/Migrations/20250930010252_Initial.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace PrimerParcialDLJAPI.Migrations
 {
     /// <inheritdoc />
-    public partial class Initial : Migration
+    public partial class InitialSupportTickets : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
